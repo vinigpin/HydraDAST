b20cfc855812a82363ad0fc2aad7127191063a11
Vinicius Pinheiro <vinicius.gdpinheiro@gmail.com>
2026-02-23 18:08:43 -0300

    mexendo no react

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,2 +1,28 @@
-import fastAPI
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+app = FastAPI()
+#python -m uvicorn api:app --reload
+
+# Configuração do CORS
+origins = [
+    "http://localhost:3000", # Porta padrão do React
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"], # Permite todos os métodos (GET, POST, etc)
+    allow_headers=["*"], # Permite todos os cabeçalhos
+)
+
+@app.get("/")
+async def root():
+    return {"mensagem": "Hello world!"}
+
+@app.get("/api/dados")
+async def get_dados():
+    return {"mensagem": "Conectado com sucesso ao FastAPI!"}
+
+    
---
+++ frontend/src/pages/Scrapme.jsx
@@ -0,0 +1,8 @@
+export default function Scrapme() {
+  return (
+    <div>
+      <h1>Esta é a página Sobre</h1>
+      <p>Bem-vindo à minha nova rota!</p>
+    </div>
+  );
+}
